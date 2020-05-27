8cab4f04a5080b19780fa8f57e4dd56b48b25047
DREBIZHEK <60065012+DREBIZHEK@users.noreply.github.com>
2020-05-27 05:30:58 +0400

    Add files via upload

---
+++ 1.1.8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			int[,,] array = new int[10, 10, 10];
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
+					for (int z = 0; z < 10; z++)
+					{
+						array[i, j, z] = rnd.Next(-1000, 1000);
+					}
+			Console.WriteLine("Исходный массив: ");
+			foreach (var item in array)
+			{
+				Console.Write($"{item} ");
+			}
+			Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------------------\nИзмененный массив:");
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
+					for (int z = 0; z < 10; z++)
+					{
+						if (array[i, j, z] > 0)
+						{
+							array[i, j, z] = 0;
+							Console.Write($"{array[i, j, z]} ");
+						}
+						else
+						{
+							Console.Write($"{array[i, j, z]} ");
+						}
+					}
+
+
+		}
+	}
+}
---
+++ 1.2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int counter = 0;
+			string input = Console.ReadLine();
+			string[] words = input.Split(new char[] { ' ', '.', ',', ':', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (words[i] == words[i].ToLower())
+				{
+					counter++;
+				}
+			}
+			Console.WriteLine("Количество слов, начинающихся с маленькой буквы = {0}", counter);
+		}
+	}
+}
