f1065aab4ae958ed8d05346c604d87c0733b3923
DREBIZHEK <60065012+DREBIZHEK@users.noreply.github.com>
2020-05-27 05:34:55 +0400

    Add files via upload

---
+++ 1.1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a:");
+			int a = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter b:");
+			int b = int.Parse(Console.ReadLine());
+			if (a > 0 & b > 0)
+			{
+				Console.WriteLine("Area = {0}", a * b);
+			}
+			else
+			{
+				Console.WriteLine("Invalid data");
+			}
+
+		}
+	}
+}
---
+++ 1.1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter N");
+			int N = int.Parse(Console.ReadLine());
+			if (N > 0)
+			{
+				for (int i = 1; i <= N; i++)
+				{
+					for (int j = 0; j < i; j++)
+					{
+						Console.Write("*");
+					}
+					Console.WriteLine();
+				}
+			}
+		}
+	}
+}
