d84ed5022fe4669f2d789e37180347dfc7ad6ce3
DREBIZHEK <60065012+DREBIZHEK@users.noreply.github.com>
2020-09-18 17:27:33 +0400

    Add files via upload

---
+++ Task 7/7.1/7.1.1/User.Common/DependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.DAL;
+using Users.DA.Interfaces;
+using Users.BLL;
+using Users.BLL.Interfaces;
+
+namespace User.Common
+{
+    public class DependencyResolver
+    {
+        private static readonly IUserLogic _usersLogic;
+        private static readonly IUsersDao _usersDao;
+
+        public static IUserLogic UserLogic => _usersLogic;
+
+
+        public static IUsersDao UserDao => _usersDao;
+
+        static DependencyResolver()
+        {
+            _usersDao = new UserDao();
+            _usersLogic = new UserLogic(_usersDao);
+        }
+    }
+}
+
