73eb1f4800e06e6ad1e45c521acb8aa8eeed4955
DREBIZHEK <bogdanmosenz@gmail.com>
2020-09-18 17:57:12 +0400

    no message

--- 2.1.2/Figure.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _2._1._2
-{
-	public abstract class Figure
-	{
-		protected double centreX, centreY;
-		public abstract void Show();
-	}
-}
---
+++ Task 1 & 2/1.1.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter N:");
+			int N = 2 * int.Parse(Console.ReadLine());
+			int count = 1;
+			for (int i = 0; i < N; N -= 2)
+			{
+				for (int j = 0; j < N / 2; j++)
+				{
+					Console.Write(" ");
+				}
+				for (int j = 0; j < count; j++)
+				{
+					Console.Write("*");
+				}
+				count += 2;
+				Console.WriteLine();
+			}
+		}
+	}
+}
